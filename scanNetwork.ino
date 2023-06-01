ec962d38bd7f61872012cda43754808bd899fee1
Mirumo <mysteryperson520@gmail.com>
2023-06-01 13:39:53 +0800

    update

--- scanNetwork.ino
+++ scanNetwork.ino
@@ -1,5 +1,6 @@
 /*
 
+<<<<<<< Updated upstream
  This example  prints the Wifi shield's MAC address, and
  scans for available Wifi networks using the Wifi shield.
  Every ten seconds, it scans again. It doesn't actually
@@ -113,4 +114,42 @@ void printEncryptionType(int thisType) {
       Serial.println("Auto");
       break;
   }
+=======
+#include <WiFiNINA.h>
+
+int status=WL_IDLE_STATUS;
+char ssid[]="iPhone CF";
+char pass[]="cf205666";
+
+
+
+void setup() {
+  
+int i=1;  
+Serial.begin(9600);
+pinMode(LED_BUILTIN,OUTPUT);
+ digitalWrite(LED_BUILTIN,LOW);
+
+while (status!=WL_CONNECTED && i<=10) {
+  Serial.print("嘗試連線第 ");
+  Serial.print(i);
+  Serial.println("次...");
+  
+  status=WiFi.begin(ssid,pass);
+
+  if (status!=WL_CONNECTED)
+     Serial.println("     無法連線");
+  else {
+     Serial.println("     成功連線");
+     digitalWrite(LED_BUILTIN,HIGH);
+  }
+  i++;
+  delay(5000);
+}
+
+}
+
+void loop() {
+ 
+>>>>>>> Stashed changes
 }
